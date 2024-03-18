a6dc5b5007d88c104ff077261f36b9788fc1d55a
Kate Fim <trifoliuvv@gmail.com>
2024-03-18 09:11:44 +0300

    start v2

--- scripts/index.js
+++ scripts/index.js
@@ -19,18 +19,21 @@ const container = document.querySelector('.content'),
 class Card {
     wrapper = cardContainer;
     static storage_array = [];
-    constructor(template, content) {
+
+    constructor(template, content, id) {
+        this.id = id;
         this.template = template;
         this.element = this.template.querySelector('.card').cloneNode(true);
         this.content = content;
     }
 
     createCard() {
+        this.element.dataset.id = `${this.id}`;
         this.element.querySelector('.card__title').textContent = this.content.name;
         this.element.querySelector('.card__image').src = this.content.link;
         this.element.querySelector('.card__image').alt = 'это фотография ' + this.content.name;
-        this.addListeners();
         this.wrapper.append(this.element);
+        this.addListeners();
 
         Card.storage_array.push({
             name: `${this.element.querySelector('.card__title').textContent}`,
@@ -40,20 +43,25 @@ class Card {
 
     addListeners() {
         this.element.querySelector('.card__delete-button').addEventListener('click', function () {
+            Card.storage_array.splice(parseInt(this.closest('.card').dataset.id), 1);
+            Card.storageUpdate();
             this.closest('.card').remove();
         });
         this.element.querySelector('.card__like-button').addEventListener('click', function () {
             this.classList.toggle('is-active');
         });
     }
+
     static storageUpdate() {
         localStorage.setItem('card_key', JSON.stringify(Card.storage_array));
     }
 }
 
-array.forEach(function (item) {
-    const exemplar = new Card(cardTemplate, item);
+array.forEach(function (item, id) {
+    const exemplar = new Card(cardTemplate, item, id);
     exemplar.createCard();
 });
 
 Card.storageUpdate();
+
+////localStorage.clear() for reset
